af24cdf2856e0b14f2b012e922bfd2ca85b7c63e
Anand Rajput <anandrajput007@gmail.com>
2018-10-05 18:16:19 +0530

    Add sample data into JSON and return all data.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Node
 ## Node JS Examples
 
-Return JSON object.
+Add sample data into JSON and return all data.
+
+### Input Json Format
+```
+    {
+        "Id": 5,
+        "Title": "Mean stack developer",
+        "Description": "3+ years of experience in NODE JS development",
+        "Experiance": "3+ Years",
+        "Location": "India"
+    }
+```
--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 const express = require('express');
+const bodyParser =  require('body-parser');
 //Consuming Data.js file
 const data = require('./data');
 const app = new express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 var port = process.env.PORT || 4040;
-app.get('/',function(req, res){
+app.get('/', function (req, res) {
     res.send('Get all Job from /jobs URL');
 });
 
-app.get('/jobs',function(req, res){
+//get all jobs
+app.get('/jobs', function (req, res) {
     return res.json(data.jobs);
 });
 
+//add job into jobs
+app.post('/jobs', function (req, res) {
+
+    console.log(req.body);
+    //model initilization
+    var Id = req.body.Id;
+    var Title = req.body.Title;
+    var Description = req.body.Description;
+    var Experiance = req.body.Experiance;
+    var Location = req.body.Location;
+    
+    var job = {
+        Id,
+        Title,
+        Description,
+        Experiance,
+        Location,
+    };
+
+    data.jobs.push(job);
+    return res.json(data.jobs);
+});
+
+
 app.listen(port, (req, res) => {
-    console.log(`Server running on Port: `+port);
+    console.log(`Server running on Port: ` + port);
 });
